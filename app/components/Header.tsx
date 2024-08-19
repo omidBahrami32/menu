a3f96fd0c0dd09d00d5eda5c4772fa56b39e7271
omid <omid102425@gmail.com>
2024-08-19 20:47:45 +0330

    Create header component.

---
+++ app/components/Header.tsx
@@ -0,0 +1,24 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import LanguageSelector from "./LanguageSelector";
+import Link from "next/link";
+import SearchModal from "./SearchModal";
+
+// TODO: fix overflow issue and add english
+
+const Header = ({ show }: { show: boolean }) => {
+  return (
+    <div className="flex flex-row gap-4 p-4">
+      <LanguageSelector />
+
+      {/* search icon and modal */}
+      <Link href={`/?show=${true}`} className="flex cursor-pointer items-center rounded-full bg-secondary p-4 backdrop-blur-xl">
+        <FontAwesomeIcon icon={faMagnifyingGlass} size="2x" color="white" />
+      </Link>
+      {show && <SearchModal/>}
+    </div>
+  );
+};
+
+export default Header;
