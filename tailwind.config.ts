d5e1e62946e7055adde406773c458f03d58f360b
omid <omid102425@gmail.com>
2024-08-19 20:41:27 +0330

    Add custom setting to tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary": "#ccd5da",
+        "secondary": "#648496",
+        "primary-forground": "#537484",
+        "secondary-foreground": "#65808e"
+      },
+      blur: {
+        sm: '6px',
       },
     },
   },
