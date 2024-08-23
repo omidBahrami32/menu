94028b476b9e8f847a419d6d458186e505b0d960
omid <omid102425@gmail.com>
2024-08-23 20:31:43 +0330

    Fix a bug in to farsi digit converter.

--- utils/tools.tsx
+++ utils/tools.tsx
@@ -1,13 +1,21 @@
 // function to convert numbers to persian digits
 export function toFarsiNumber(number: number) {
   const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  const pointIndex = number.toString().indexOf(".");
   const farsiNumber = number
     .toString()
     .split("")
-    .map((x) => farsiDigits[x])
+    .map((x) => farsiDigits[parseInt(x)])
     .join("");
   if (number < 1000) {
+    if (pointIndex >= 0) {
+      return (
+        farsiNumber.slice(0, pointIndex) +
+        "/" +
+        farsiNumber.slice(pointIndex, pointIndex + 2)
+      );
+    }
     return farsiNumber;
   }
-  return farsiNumber[0] + "/" + farsiNumber.slice(1);
+  return farsiNumber[0] + "/" + farsiNumber.slice(1, 4);
 }
